fce22ad2712bc3b5d8c51119b7f1afe6d5d689f4
Muhammad Shayan Asim <k250557@nu.edu.pk>
2025-11-13 20:48:03 +0500

    Create Task1.c

---
+++ Task1.c
@@ -0,0 +1,10 @@
+int* runningSum(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    result[0] = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        result[i] = result[i - 1] + nums[i];
+    }
+    
+    *returnSize = numsSize;
+    return result;
+}
